8aa518cae28154d78bfabfe25eb6b74a4ab56b87
Matheus Alves de Andrade <matheuseejam@gmail.com>
2025-06-28 16:41:56 -0300

    Ajuste na UI

--- ui/home.go
+++ ui/home.go
@@ -2,9 +2,9 @@ package ui
 
 import (
 	"image/color"
-	"log"
 
 	"gioui.org/app"
+	"gioui.org/io/pointer"
 	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/text"
@@ -27,9 +27,10 @@ func CreateHomeWindow(uiEvents chan<- UIEvent) {
 
 func initialState() *UIState {
 	state := new(UIState)
-	state.YoutubeChannelURLEditor = &widget.Editor{
-		SingleLine: true,
-	}
+	state.YoutubeChannelURLEditor = new(widget.Editor)
+	state.YoutubeChannelURLEditor.SingleLine = true
+	state.YoutubeChannelURLEditor.MaxLen = 60
+
 	state.YouTubeChannelClickable = new(widget.Clickable)
 	return state
 }
@@ -65,10 +66,12 @@ func run(window *app.Window, uiEvents chan<- UIEvent) error {
 
 func emitEvents(gtx layC, state *UIState, uiEvents chan<- UIEvent) {
 	if state.YouTubeChannelClickable.Clicked(gtx) {
-		log.Println("AQUI")
 		uiEvents <- UIEventSetYoutubeChannel{
 			Channel: state.YoutubeChannelURLEditor.Text(),
 		}
+	}
+	if state.YouTubeChannelClickable.Hovered() {
+		pointer.CursorPointer.Add(gtx.Ops)
 	}
 }
 
@@ -91,9 +94,15 @@ func renderYoutubeChannelInput(
 ) layout.FlexChild {
 	editor := state.YoutubeChannelURLEditor
 	editorUI := material.Editor(theme, editor, "YouTube Channel URL")
+	editorUI.LineHeight = 1.5
 
 	submit := state.YouTubeChannelClickable
 	submitUI := material.Button(theme, submit, "Set Channel")
+
+	if state.YoutubeChannelURLEditor.Text() == "" {
+		submitUI.Background = color.NRGBA{R: 200, G: 200, B: 200, A: 255}
+		submitUI.Color = color.NRGBA{R: 100, G: 100, B: 100, A: 255}
+	}
 
 	margin := layout.Inset{
 		Top:    unit.Dp(16),
@@ -106,18 +115,36 @@ func renderYoutubeChannelInput(
 		func(gtx layC) layD {
 			return margin.Layout(gtx, func(gtx layC) layD {
 				return layout.Flex{
-					Axis:    layout.Horizontal,
-					Spacing: layout.SpaceBetween,
+					Axis:      layout.Horizontal,
+					Spacing:   layout.SpaceBetween,
+					Alignment: layout.Middle,
 				}.Layout(
 					gtx,
-					layout.Rigid(
+					layout.Flexed(
+						1,
 						func(gtx layC) layD {
-							return editorUI.Layout(gtx)
+							return widget.Border{
+								Color:        color.NRGBA{R: 200, G: 200, B: 200, A: 255},
+								Width:        unit.Dp(1),
+								CornerRadius: unit.Dp(4),
+							}.Layout(gtx, func(gtx layC) layD {
+								return layout.UniformInset(6).Layout(
+									gtx,
+									func(gtx layC) layD {
+										return editorUI.Layout(gtx)
+									},
+								)
+							})
 						},
 					),
 					layout.Rigid(
 						func(gtx layC) layD {
-							return submitUI.Layout(gtx)
+							return layout.Inset{Left: unit.Dp(8)}.Layout(
+								gtx,
+								func(gtx layC) layD {
+									return submitUI.Layout(gtx)
+								},
+							)
 						},
 					),
 				)
