0eb13e3c1a3416634f02971584cf83739fb60e03
Matheus Alves de Andrade <matheuseejam@gmail.com>
2025-06-28 18:07:47 -0300

    Inicia chat stream

---
+++ chat_stream/types.go
@@ -0,0 +1,42 @@
+package chat_stream
+
+type PlatformType string
+
+const (
+	PlatformTypeYoutube PlatformType = "youtube"
+	PlatformTypeTwitch  PlatformType = "twitch"
+)
+
+const ChatStreamMessageBufferSize = 200
+
+type ChatStreamMessage struct {
+	Platform  PlatformType
+	Name      string
+	Message   string
+	Timestamp string
+}
+
+type ChatStreamCon interface {
+	IsConnected() bool
+	GetMessagesChan() <-chan ChatStreamMessage
+	Close()
+}
+
+type YTChatStreamCon struct {
+	ChannelID string
+	stream    chan ChatStreamMessage
+}
+
+func (c *YTChatStreamCon) IsConnected() bool {
+	// Placeholder for actual connection check logic
+	return c.stream != nil
+}
+func (c *YTChatStreamCon) GetMessagesChan() <-chan ChatStreamMessage {
+	return c.stream
+}
+func (c *YTChatStreamCon) Close() {
+	if c.stream != nil {
+		close(c.stream)
+		c.stream = nil // Clear the stream to prevent further messages
+	}
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"overtube/chat_stream"
 	"overtube/ui"
 	"reflect"
 )
@@ -13,11 +14,12 @@ func main() {
 }
 
 func orchestrateEvents(uiEventChan chan ui.UIEvent) {
+	var chatStream chat_stream.ChatStreamCon = nil
 	for {
 		event, more := <-uiEventChan
 		if !more {
 			log.Println("UI event channel closed")
-			return
+			break
 		}
 		if event.GetError() != nil {
 			log.Fatalln(event.GetError())
@@ -25,11 +27,43 @@ func orchestrateEvents(uiEventChan chan ui.UIEvent) {
 
 		switch v := event.(type) {
 		case ui.UIEventSetYoutubeChannel:
-			log.Println("Set channel: ", v.Channel)
+			closeChatStream(chatStream)
+			var err error = nil
+			chatStream, err = chat_stream.ConnectToYoutubeChat(v.Channel)
+			if err != nil {
+				log.Fatalln("Failed to connect to YouTube chat: ", err)
+			} else {
+				go handleChatStreamMessages(chatStream)
+			}
 		case ui.UIEventExit:
 			log.Println("User exited")
 		default:
-			log.Fatalln("Unknown event type:", reflect.TypeOf(event))
+			log.Fatalln("Unknown event type: ", reflect.TypeOf(event))
 		}
 	}
+
+	closeChatStream(chatStream)
+}
+
+func handleChatStreamMessages(chatStream chat_stream.ChatStreamCon) {
+	for {
+		if !chatStream.IsConnected() {
+			break
+		}
+		msg, more := <-chatStream.GetMessagesChan()
+		if !more {
+			log.Println("Chat stream messages channel closed")
+			break
+		}
+		log.Printf("Received message from %s on %s: %s\n", msg.Name, msg.Platform, msg.Message)
+	}
+}
+
+func closeChatStream(chatStream chat_stream.ChatStreamCon) {
+	if chatStream == nil || !chatStream.IsConnected() {
+		return
+	}
+	log.Print("Closing chat stream connection...")
+	chatStream.Close()
+	log.Print("Chat Stream closed!")
 }
